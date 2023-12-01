55393dadb47d33c625a915f1b5e6654fd74f3d68
yooyoungpark <yoongtopiaa@gmail.com>
2023-12-01 15:01:35 +0900

    feat : Lotto 클래스 생성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,39 @@
 package lotto;
 
+import static lotto.Application.LOTTO_NUMBER_DIGIT;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final List<LottoNumber> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(List<LottoNumber> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validate(List<LottoNumber> numbers) {
+        if (isDuplicated(numbers)) {
+            throw new IllegalArgumentException("중복 되지 않는 수를 입력해 주세요.");
+        }
+        if (numbers.size() != LOTTO_NUMBER_DIGIT) {
+            throw new IllegalArgumentException("로또 번호는 " + LOTTO_NUMBER_DIGIT + "개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean isDuplicated(List<LottoNumber> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            for (int j = i + 1; j < numbers.size(); j++) {
+                if (numbers.get(i).equals(numbers.get(j))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public List<LottoNumber> getNumbers() {
+        return numbers;
+    }
 }
